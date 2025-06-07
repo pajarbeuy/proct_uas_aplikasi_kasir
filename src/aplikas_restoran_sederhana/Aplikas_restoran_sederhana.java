07f6f18338763e9c9550de78c9316a14b84ec742
ACER <ACER@DESKTOP-C6ATO7I>
2025-06-07 16:57:21 +0700

    first Commit!

---
+++ src/aplikas_restoran_sederhana/Aplikas_restoran_sederhana.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikas_restoran_sederhana;
+
+/**
+ *
+ * @author ACER
+ */
+public class Aplikas_restoran_sederhana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
